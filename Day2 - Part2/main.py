28749424256d57b8e1346e8809780f04b3b65d12
Jordi Garcia <jgarcia@ackcent.com>
2021-12-02 13:08:03 +0100

    Second day

---
+++ Day2 - Part2/main.py
@@ -0,0 +1,18 @@
+f = open("input.txt", "r")
+lines = f.readlines()
+
+coordinates=[0,0]
+aim=0
+
+for i in range(len(lines)):
+    command = lines[i].split(" ")
+    if command[0] == "forward":
+        coordinates[0]+=int(command[1])
+        coordinates[1]+=(int(command[1])*aim)
+    elif command[0] == "up":
+        aim-=int(command[1])
+    elif command[0] == "down":
+        aim+=int(command[1])
+    
+
+print(f"Result = {coordinates[0]} * {coordinates[1]} = {coordinates[0]*coordinates[1]}")
